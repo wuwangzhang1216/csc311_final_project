fa4ce602a13ae8a1924be389107173c83a284547
Richard Huang <hxr.richard@gmail.com>
2021-12-02 22:20:22 -0500

    vectorization completly done

--- part_a/item_response.py
+++ part_a/item_response.py
@@ -1,17 +1,29 @@
+import os
+import sys
+
+# Adding parent directory to path (for importing utils)
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+# change current working directory to part_a to access data folder
+os.chdir("./part_a")
 from utils import *
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+N_STUDENTS = 542
+N_QUESTIONS = 1774
+
 
 def sigmoid(x):
-    """ Apply sigmoid function.
-    """
+    """Apply sigmoid function."""
     return np.exp(x) / (1 + np.exp(x))
 
 
-def neg_log_likelihood(data, theta, beta):
-    """ Compute the negative log-likelihood.
+def neg_log_likelihood(data, theta, beta, C_mat, data_mask):
+    """Compute the negative log-likelihood.
 
     You may optionally replace the function arguments to receive a matrix.
 
@@ -27,32 +39,30 @@ def neg_log_likelihood(data, theta, beta):
     #####################################################################
 
     # initialize some variables
-    user_id = data["user_id"]
-    question_id = data["question_id"]
-    is_correct = data["is_correct"]
+    capability = np.subtract.outer(theta, beta)  # (theta_i - beta_j) matrix
+    
+    log_lklihood_mat = (C_mat * capability - np.logaddexp(0, capability)) * data_mask
+    log_lklihood = np.sum(log_lklihood_mat)
+    return -log_lklihood
 
-    log_lklihood = 0
-    for i in range(len(is_correct)):
-        log_lklihood += \
-            is_correct[i] * (theta[user_id[i]] - beta[question_id[i]]) - \
-            np.logaddexp(0, theta[user_id[i]] - beta[question_id[i]])
-        # # if c_ij = 1
-        # if is_correct[i] == 1:
-        #     log_lklihood += theta[user_id[i]] - beta[question_id[i]] \
-        #                     - np.logaddexp(0, theta[user_id[i]] -
-        #                                    beta[question_id[i]])
-        # # if c_ij = 0
-        # else:
-        #     log_lklihood += - np.logaddexp(0, theta[user_id[i]] -
-        #                                    beta[question_id[i]])
+
+    # user_id = data["user_id"]
+    # question_id = data["question_id"]
+    # is_correct = data["is_correct"]
+
+    # log_lklihood = 0
+    # for x in range(len(is_correct)):
+    #     i, j, c = user_id[x], question_id[x], is_correct[x]
+    #     capability = theta[i] - beta[j]
+    #     log_lklihood += c * capability - np.logaddexp(0, capability)
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
     return -log_lklihood
 
 
-def update_theta_beta(data, lr, theta, beta):
-    """ Update theta and beta using gradient descent.
+def update_theta_beta(data, lr, theta, beta, C_mat, data_mask):
+    """Update theta and beta using gradient descent.
 
     You are using alternating gradient descent. Your update should look:
     for i in iterations ...
@@ -74,24 +84,34 @@ def update_theta_beta(data, lr, theta, beta):
     #####################################################################
 
     # initialize some variables
-    user_id = data["user_id"]
-    question_id = data["question_id"]
-    is_correct = data["is_correct"]
+    t, b = theta.reshape(-1,), beta.reshape(
+        -1,
+    )
 
-    theta_gradient = np.zeros(len(theta))
-    beta_gradient = np.zeros(len(beta))
+    capability = np.subtract.outer(t, b)  # (theta_i - beta_j) matrix
+    sigmoid_cap = sigmoid(capability)
+    d_theta_mat = C_mat - sigmoid_cap
+    d_beta = (-d_theta_mat * data_mask).T @ np.ones((N_STUDENTS))
+    d_theta = (d_theta_mat * data_mask) @ np.ones((N_QUESTIONS))
 
-    for i in range(len(is_correct)):
-        # compute the derivatives
-        temp = sigmoid(theta[user_id[i]] - beta[question_id[i]])
-        grad_theta = is_correct[i] - temp
-        grad_beta = - grad_theta
-        # apply the update to theta_gradient/beta_gradient
-        theta_gradient[user_id[i]] += grad_theta
-        beta_gradient[question_id[i]] += grad_beta
-    # apply the update
-    theta += lr * theta_gradient
-    beta += lr * beta_gradient
+    theta += lr * d_theta
+    beta += lr * d_beta
+    return theta, beta
+
+    # theta_gradient = np.zeros(len(theta))
+    # beta_gradient = np.zeros(len(beta))
+
+    # for i in range(len(is_correct)):
+    #     # compute the derivatives
+    #     temp = sigmoid(theta[user_id[i]] - beta[question_id[i]])
+    #     grad_theta = is_correct[i] - temp
+    #     grad_beta = -grad_theta
+    #     # apply the update to theta_gradient/beta_gradient
+    #     theta_gradient[user_id[i]] += grad_theta
+    #     beta_gradient[question_id[i]] += grad_beta
+    # # apply the update
+    # theta += lr * theta_gradient
+    # beta += lr * beta_gradient
 
     #####################################################################
     #                       END OF YOUR CODE                            #
@@ -99,8 +119,8 @@ def update_theta_beta(data, lr, theta, beta):
     return theta, beta
 
 
-def irt(data, val_data, lr, iterations):
-    """ Train IRT model.
+def irt(data, val_data, lr, iterations, C_mat, data_mask, C_mat_val, data_mask_val):
+    """Train IRT model.
 
     You may optionally replace the function arguments to receive a matrix.
 
@@ -113,8 +133,9 @@ def irt(data, val_data, lr, iterations):
     :return: (theta, beta, val_acc_lst)
     """
     # TODO: Initialize theta and beta.
-    theta = np.random.rand(542)  # num of students
-    beta = np.random.rand(1774)  # num of questions
+    theta = np.random.rand(N_STUDENTS)
+    beta = np.random.rand(N_QUESTIONS)
+
 
     val_acc_lst = []
     data_acc_lst = []
@@ -122,23 +143,25 @@ def irt(data, val_data, lr, iterations):
     train_like_lst = []
 
     for i in range(iterations):
-        neg_lld_train = neg_log_likelihood(data, theta=theta, beta=beta)
-        neg_lld_val = neg_log_likelihood(val_data, theta=theta, beta=beta)
-        score_data = evaluate(data=data, theta=theta, beta=beta)
-        score_val = evaluate(data=val_data, theta=theta, beta=beta)
+        t = theta.reshape(-1, )
+        b = beta.reshape(-1, )
+        neg_lld_train = neg_log_likelihood(data, t, b, C_mat, data_mask)
+        neg_lld_val = neg_log_likelihood(data, t, b, C_mat_val, data_mask_val)
+        score_data = evaluate(data, t, b, C_mat, data_mask)
+        score_val = evaluate(val_data, t, b, C_mat_val, data_mask_val)
         val_acc_lst.append(score_val)
         data_acc_lst.append(score_data)
         val_like_lst.append(-neg_lld_val)
         train_like_lst.append(-neg_lld_train)
-        # print("NLLK: {} \t Score: {}".format(neg_lld, score))
-        theta, beta = update_theta_beta(data, lr, theta, beta)
+        print(f"NLLK iter={i}:\t {neg_lld_train} \t Score: {score_data}")
+        theta, beta = update_theta_beta(data, lr, theta, beta, C_mat, data_mask)
 
     # TODO: You may change the return values to achieve what you want.
     return theta, beta, val_acc_lst, data_acc_lst, val_like_lst, train_like_lst
 
 
-def evaluate(data, theta, beta):
-    """ Evaluate the model given data and return the accuracy.
+def evaluate(data, theta, beta,C_mat, data_mask):
+    """Evaluate the model given data and return the accuracy.
     :param data: A dictionary {user_id: list, question_id: list,
     is_correct: list}
 
@@ -146,15 +169,40 @@ def evaluate(data, theta, beta):
     :param beta: Vector
     :return: float
     """
-    pred = []
-    for i, q in enumerate(data["question_id"]):
-        u = data["user_id"][i]
-        x = (theta[u] - beta[q]).sum()
-        p_a = sigmoid(x)
-        pred.append(p_a >= 0.5)
-    return np.sum((data["is_correct"] == np.array(pred))) \
-           / len(data["is_correct"])
+    capability = np.subtract.outer(theta, beta)
+    sigmoid_cap = sigmoid(capability) 
+    pred_mat = (sigmoid_cap >= 0.5) 
+    pred_correct = np.sum((pred_mat == C_mat) * data_mask)
 
+    return pred_correct / np.sum(data_mask)
+    # pred = []
+    # for i, q in enumerate(data["question_id"]):
+    #     u = data["user_id"][i]
+    #     x = (theta[u] - beta[q]).sum()
+    #     p_a = sigmoid(x)
+    #     pred.append(p_a >= 0.5)
+    # return np.sum((data["is_correct"] == np.array(pred))) / len(data["is_correct"])
+
+def build_data_mat(data):
+    """Build C_mat and datamask for the given data
+
+    :param data: A dictionary {user_id: list, question_id: list,
+    is_correct: list}
+    """
+    C_mat = np.zeros((N_STUDENTS, N_QUESTIONS))
+    data_mask = np.zeros((N_STUDENTS, N_QUESTIONS))
+
+    users = data["user_id"]
+    questions = data["question_id"]
+    is_correct = data["is_correct"]
+    for i in range(len(users)):
+        u = users[i]
+        q = questions[i]
+        c = is_correct[i]
+
+        C_mat[u, q] = c
+        data_mask[u, q] = 1
+    return C_mat, data_mask
 
 def main():
     train_data = load_train_csv("../data")
@@ -171,7 +219,14 @@ def main():
     learning_rate = 0.001
     number_of_iteration = 150
     iteration_list = [*range(1, number_of_iteration + 1, 1)]
-    theta, beta, val_acc_list, train_acc_list, val_like_lst, train_like_lst = irt(train_data, val_data, learning_rate, number_of_iteration)
+
+    C_mat, data_mask = build_data_mat(train_data)
+    C_mat_val, data_mask_val = build_data_mat(val_data)
+    C_mat_test, data_mask_test = build_data_mat(test_data)
+
+    theta, beta, val_acc_list, train_acc_list, val_like_lst, train_like_lst = irt(
+        train_data, val_data, learning_rate, number_of_iteration, C_mat, data_mask, C_mat_val, data_mask_val
+    )
 
     # report the validation and test accuracy
     plt.plot(val_acc_list, label="validation accuracy")
@@ -189,16 +244,16 @@ def main():
 
     plt.xlabel("iteration number")
     plt.ylabel("likelihood")
-    plt.title('log-likelihood of training and validation VS num of iteration')
+    plt.title("log-likelihood of training and validation VS num of iteration")
     plt.legend()
     plt.show()
 
     # part c
     # report the final validation and test accuracies
-    valid_acc = evaluate(val_data, theta, beta)
-    test_acc = evaluate(test_data, theta, beta)
-    print("The final accuracy for validation set is {}".format(valid_acc))
-    print("The final accuracy for test set is {}".format(test_acc))
+    valid_acc = evaluate(val_data, theta, beta, C_mat_val, data_mask_val)
+    test_acc = evaluate(test_data, theta, beta, C_mat_test, data_mask_test)
+    print(f"The final accuracy for validation set is {valid_acc}")
+    print(f"The final accuracy for test set is {test_acc}")
 
     #####################################################################
     #                       END OF YOUR CODE                            #
@@ -210,17 +265,12 @@ def main():
     for j in range(1, 4):
         sorted_theta_list = np.sort(theta)
         p_correct = np.exp(-np.logaddexp(0, beta[j] - sorted_theta_list))
-        plt.plot(sorted_theta_list, p_correct, label="j_{}".format(j))
+        plt.plot(sorted_theta_list, p_correct, label=f"j_{j}")
     plt.title("correct response vs theta")
     plt.ylabel("probability")
     plt.xlabel("theta")
     plt.legend()
     plt.show()
-    #####################################################################
-    pass
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
 
 
 if __name__ == "__main__":
